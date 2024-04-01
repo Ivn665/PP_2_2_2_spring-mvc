1713248536b3a7f02f709643c39de6ded0d68b7c
Ivn665 <157416939+Ivn665@users.noreply.github.com>
2024-04-01 17:09:17 +0300

    Практическая задача 2.2.2 Java pre-project. Задача 2.2.2. MVC-приложение.

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 
 @Controller
@@ -22,12 +24,11 @@ public class CarsController {
     }
 
     @GetMapping()
-    public String printCarsRange(HttpServletRequest request, Model model) {
-        try {
-            int quantity = Integer.parseInt(request.getParameter("count"));
-            model.addAttribute("cars", carService.getCarsByQuantity(quantity));
-        } catch (NumberFormatException e) {
+    public String printCarsRange(@RequestParam(value = "count", required = false) Optional<Integer> quantity, Model model) {
+        if (quantity.isEmpty()) {
             model.addAttribute("cars", carService.getCars());
+        } else {
+            model.addAttribute("cars", carService.getCarsByQuantity(quantity.get()));
         }
         return "cars";
     }
