7c7f22ae08175747702717b67c705d1e1b3af174
Ivn665 <157416939+Ivn665@users.noreply.github.com>
2024-04-01 20:27:21 +0300

    Сделал видимой сетку таблицы

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 
